1108d413178d28b3380cf4cd2d96ba720a72027a
Ryan McGill <ryanjamesmcgill@gmail.com>
2018-12-15 15:35:05 -0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// replace this file with data in https://paper.dropbox.com/doc/Congress-Commentator-Config--ATwHo6jeqCx0nBut_EiLAZ7eAg-xYEzMfpbVCa7PU3lziVFq
+
+
+// test account: https://twitter.com/RWilliamsVotes
+exports.twitterKeys = {
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+}
+
+exports.propublicaKeys = {
+  datastore: ''
+}
