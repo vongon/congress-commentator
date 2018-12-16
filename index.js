3bf6dbdda4b3f1d28fba1777e7828b6659a800d3
Matt Oliver <mattoliver.mattoliver@gmail.com>
2018-12-16 13:37:53 -0600

    pulling data from our example file just fine

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ main = (cb) => {
 		  throw err
 		  console.log('There was an error with the handleBillData function.')
 		} 
-		// Parse the buffer that fs.readFile creates
+		// Parse the buffer that fs.readFile creates and store it in a variable we can use
 		var proPublicaData = JSON.parse(data)
 
 		// 'votes' is an array and here we are just returning the first index; 
 		var mostRecentBill = proPublicaData.results[0].votes[0]
 
 		// this is the stuff we want to tweet
-		console.log ('Using fs.readFile to read the bill data! Here is the most recent bill action was taken on: '+ mostRecentBill.bill.number + '. Most recent vote: ' + mostRecentBill["position"])
+		console.log ('Using fs.readFile to read the bill data! Roger Williams member ID is: ' + proPublicaData.results[0].member_id + '. Here is the most recent bill action was taken on: '+ mostRecentBill.bill.number + '. Most recent vote: ' + mostRecentBill["position"])
 		
 		// trying to throw something into a variable we can tweet...but....		
 		var params = { status: 'Most recent vote: ' +  mostRecentBill.bill.number }
