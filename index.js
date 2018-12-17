9c71f0816451cb9301a5bc763cb5a422759eedde
Matt Oliver <mattoliver.mattoliver@gmail.com>
2018-12-16 22:44:41 -0600

    phrasing tweet as unambiguously as possible and trying to fit all relevant info in

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ main = (cb) => {
 	function handleBillData(err, data) {
 		if (err) {
 		  throw err
-		  console.log('There was an error with the handleBillData function.')
 		} 
 		// Parse the buffer that fs.readFile creates and store it in a variable we can use
 		var proPublicaData = JSON.parse(data)
@@ -24,14 +23,23 @@ main = (cb) => {
 		// 'votes' is an array and here we are just returning the first index; 
 		var mostRecentBill = proPublicaData.results[0].votes[0]
 
+		var billsArray = proPublicaData.results[0].votes 
+
 		// date of the vote
 		var voteDate = proPublicaData.results[0].votes[0].date
+		var abbreviatedBillQuestion = proPublicaData.results[0].votes[0].question.substring(0,30)
+		var abbreviatedBillDescription = mostRecentBill.description.substring(0,65)
 
-		// this is the stuff we want to tweet
+
+		// Logging some basics to the console
 		console.log ('Using fs.readFile to read the bill data. Roger Williams member ID is: ' + proPublicaData.results[0].member_id + '. Here is the most recent bill action was taken on: '+ mostRecentBill.bill.number + '. Most recent vote: ' + mostRecentBill["position"])
 		
-		// storing the data we want to tweet in a variable		
-		var params = 'On ' + voteDate + 'Roger Williams voted ' + mostRecentBill["position"] + 'on ' mostRecentBill.bill.number + ', ' + '"' mostRecentBill.description.substring(0,65) + '". ' + mostRecentBill.total["yes"] + 'members voted "Yes", ' +  mostRecentBill.total["no"] + 'members voted "No", ' + mostRecentBill.total["not_voting"] + 'not voting.' 
+		// storing the data we want to tweet in a variable -- once deployed, member's Twitter handle should be used	
+		var params = '"' + abbreviatedBillQuestion + '" on ' + mostRecentBill.bill.number + 
+		', Roger Williams (R-Weatherford) voted "' + mostRecentBill["position"] + '". \n\n Description: "' + 
+		abbreviatedBillDescription +'".\n\n' + mostRecentBill.total["yes"] + ' member(s) voted "Yes".\n' 
+		+  mostRecentBill.total["no"] + ' voted "No".\n' + mostRecentBill.total["not_voting"] 
+		+ ' not voting.\n Result: ' + mostRecentBill.result + ' ' + voteDate + '.' 
 		
 		// tweeting
 		twitterService.tweet(params, function (err, data, response) {
@@ -50,4 +58,14 @@ tweetBill();
 main((err) => {
   if (err) throw JSON.stringify(err);
   console.log('DONE!');
+
+
 });
+
+
+// var params = 'On ' + voteDate + ', Roger Williams voted "' + mostRecentBill["position"] 
+// 		+ '"" on ' + billQuesion + mostRecentBill.bill.number + '. \n' + 'Description: "' 
+// 		+ billDescription + '". \n' + mostRecentBill.total["yes"] + ' member(s) voted "Yes". \n ' 
+// 		+  mostRecentBill.total["no"] + ' member(s) voted "No". \n\n ' + mostRecentBill.total["not_voting"] 
+// 		+ ' member(s) not voting.\n\n The bill ' + mostRecentBill.result + ' in the ' + mostRecentBill.chamber + '.' 
+// 		
