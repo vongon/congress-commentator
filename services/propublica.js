ee2c95744cb80136e1efb8948aa2cc56887bc1a0
Matt Oliver <mattoliver.mattoliver@gmail.com>
2018-12-17 23:18:00 -0600

    reading latest bill data from the Pro Publica API memberVotePositions() endpoint now instead of using fs.readFile

--- services/propublica.js
+++ services/propublica.js
@@ -5,15 +5,24 @@ var Congress = require( 'propublica-congress-node' );
 var client = new Congress( propublicaKeys.datastore );
 
 exports.getLatestVoteData = getLatestVoteData = (cb) =>
-  client.memberLists({
-    congressNumber: '114', // we should move this data to config.js
-    chamber: 'house'
+  // client.memberLists({
+  //   congressNumber: '115', // we should move this data to config.js
+  //   chamber: 'house'
+  // }).then(function(res) {
+  //   return cb(null, res);
+  // }).catch((err) => {
+  //   return cb(err);
+  // });
+
+  // just looking for the votes taken by this specific member; so use this endpoint
+  client.memberVotePositions({
+    memberId: 'W000816'
   }).then(function(res) {
     return cb(null, res);
   }).catch((err) => {
-    return cb(err);
+    return cb(err)
   });
-
+  
 if(!module.parent) {
   getLatestVoteData((err, res) => {
     if (err) throw err;
