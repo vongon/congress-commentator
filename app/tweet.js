9d50441ea7d0d45ce5221379a526f5df9fde73d4
Matt Oliver <mattoliver.mattoliver@gmail.com>
2018-12-26 22:25:03 -0600

    wip

--- app/tweet.js
+++ app/tweet.js
@@ -8,6 +8,7 @@ const Contribution = require('../models/contribution');
 module.exports = tweet = (cb) => {
 
   const fecMessage = getFECTweetString(cb);
+
   console.log('Tweeting:', fecMessage);
   
   twitterService.tweet(fecMessage, (err) => {
@@ -16,6 +17,7 @@ module.exports = tweet = (cb) => {
     }
     console.log('Successfully test tweeted.')
     }); 
+
 };
 
 const getFECTweetString = (data) => {
@@ -23,14 +25,13 @@ const getFECTweetString = (data) => {
   const name = config.congressPerson.name;
   const party = config.congressPerson.party;
   const jurisdiction = config.congressPerson.jurisdiction;
+
   
-  const upToDate = data.date_coverage_to
-  const pacMoney = data.total_from_pacs
+  const upToDate = data.date_coverage_to // <-- need to get results of importData here somehow
+  const pacMoney = data.total_from_pacs // <-- shouldn't using async.series in index.js be passing the results of importData to the tweet function?
 
   // const fecMessage = `Testing FEC API.`
-  const fecMessage = `Testing FEC API.
-As of ${upToDate}, ${name} (${party}-${jurisdiction}) had accepted $${pacMoney} from PACs.
-  `;
+  const fecMessage = `Testing FEC API. As of ${upToDate}, ${name} (${party}-${jurisdiction}) had accepted $${pacMoney} from PACs.`;
   return fecMessage;
 }
 
