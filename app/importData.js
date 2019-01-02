594b3c3f127945edd4029819b9b5c2cfe6d0025e
Matt Oliver <mattoliver.mattoliver@gmail.com>
2019-01-02 00:17:08 -0600

    updating variable names, not skipping items in db though

--- app/importData.js
+++ app/importData.js
@@ -11,20 +11,20 @@ module.exports = importData = (cb) => {
         // get votes
         propublicaService.getLatestVoteData((err, data) => {
           if (err) {
-            return voteCb(err);
+            return sCb(err);
           }
           const votes = data.votes;
-          async.eachSeries(votes, (data, voteCb) => {
-            Vote.find({'data.vote_uri': data.vote_uri}).lean(true).exec((err, votes) => {
+          async.eachSeries(votes, (vote, voteCb) => {
+            Vote.find({'votes.vote_uri': vote.vote_uri}).lean(true).exec((err, votes) => {
               if (err) {
                 return voteCb(err);
               } 
               if (votes.length > 0) {
-                console.log('Skipping vote uploaded because found existing entry for vote_uri:', data.vote_uri);
+                console.log('Skipping vote uploaded because found existing entry for vote_uri:', vote.vote_uri);
                 return setImmediate(voteCb);
               }
-              console.log('Uploading new vote_uri:', data.vote_uri);
-              const newVote = new Vote({data});
+              console.log('Uploading new vote_uri:', vote.vote_uri);
+              const newVote = new Vote({vote});
               newVote.save(voteCb);
             });
           }, sCb);
@@ -35,23 +35,23 @@ module.exports = importData = (cb) => {
         // get FEC data
         propublicaService.getCampaignFinanceData((err, data) => {
           if (err) {
-            return contributionsCb(err);
+            return sCb(err);
           }
         
           const contributions = data.results;
-          async.eachSeries(contributions, (data, contributionsCb) => {
-            Contribution.find({'data.fec_uri': data.fec_uri}).lean(true).exec((err, contributions) => {
+          async.eachSeries(contributions, (contribution, contributionCb) => {
+            Contribution.find({'contribution.fec_uri': contribution.fec_uri}).lean(true).exec((err, contributions) => {
               if (err) {
-                return contributionsCb(err);
+                return contributionCb(err);
               }
 
               if (contributions.length > 0) {
-                console.log('Skipping this contribution upload becase we have an existing entry for fec_uri:', data.fec_uri);
-                return setImmediate(contributionsCb);
+                console.log('Skipping this contribution upload becase we have an existing entry for fec_uri:', contribution.fec_uri);
+                return setImmediate(contributionCb);
               }
-              console.log('Uploading new fec_uri:', data.fec_uri);
-              const newContribution = new Contribution({data});
-              newContribution.save(contributionsCb);
+              console.log('Uploading new fec_uri:', contribution.fec_uri);
+              const newContribution = new Contribution({contribution});
+              newContribution.save(contributionCb);
             })
           }, sCb)
         }); // end getCampaignFinanceData
