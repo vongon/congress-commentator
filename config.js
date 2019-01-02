bca0456fdef3b5be349a18f2446434ed2851ea08
Ryan James McGill <ryanjamesmcgill@gmail.com>
2019-01-01 18:09:49 -0800

    add customconfig options

--- config.js
+++ config.js
@@ -1,34 +1,36 @@
 // replace this file with data in https://paper.dropbox.com/doc/Congress-Commentator-Config--ATwHo6jeqCx0nBut_EiLAZ7eAg-xYEzMfpbVCa7PU3lziVFq
 
+if (process.env.configPath) {
+  module.exports = require(process.env.configPath);
+  return;
+}
+
 // test account: https://twitter.com/RWilliamsVotes
-exports.congressPerson = {
-  name: '', // Roger Williams 
-  party: '', // R
-  jurisdiction: '', // Weatherford
-  handle: '', // @RepRWilliams
-}
-
-exports.twitterKeys = {
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: '',
-}
-
-// find member id here: 'http://bioguide.congress.gov/biosearch/biosearch.asp'
-exports.propublicaKeys = {
-  memberId: '' ,
-  datastore: '',
-  
-}
-
-exports.mongo = {
-  connectionString: '',
-}
-
-// campaign finance keys
-exports.propublicaFEC = {
-  fec_key: '',
-  url: '',
-  fec_id: '', // https://www.fec.gov/data/browse-data/?tab=candidates
-}
+module.exports = {
+  congressPerson: {
+    name: '', // Roger Williams 
+    party: '', // R
+    jurisdiction: '', // Weatherford
+    handle: '', // @RepRWilliams
+  },
+  twitterKeys: {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: '',
+  },
+  // find member id here: 'http://bioguide.congress.gov/biosearch/biosearch.asp'
+  propublicaKeys: {
+    memberId: '' ,
+    datastore: '',
+  },
+  // campaign finance keys
+  propublicaFEC: {
+    fec_key: '',
+    url: '',
+    fec_id: '', // https://www.fec.gov/data/browse-data/?tab=candidates
+  },
+  mongo: {
+    connectionString: '',
+  }
+};
