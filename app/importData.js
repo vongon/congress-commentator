3cbb12bf776999fc0b7ca2649e60c1190a7ad8d5
Matt Oliver <mattoliver.mattoliver@gmail.com>
2019-01-02 23:30:13 -0600

    correcting variable names and making requested changes :construction:

--- app/importData.js
+++ app/importData.js
@@ -14,8 +14,8 @@ module.exports = importData = (cb) => {
             return sCb(err);
           }
           const votes = data.votes;
-          async.eachSeries(votes, (vote, voteCb) => {
-            Vote.find({'votes.vote_uri': votes.vote_uri}).lean(true).exec((err, votes) => {
+          async.eachSeries(data.votes, (vote, voteCb) => {
+            Vote.find({'data.vote_uri': data.vote_uri}).lean(true).exec((err, votes) => {
               if (err) {
                 return voteCb(err);
               } 
@@ -23,8 +23,8 @@ module.exports = importData = (cb) => {
                 console.log('Skipping vote uploaded because found existing entry for vote_uri:', vote.vote_uri);
                 return setImmediate(voteCb);
               }
-              console.log('Uploading new vote_uri:', votes.vote_uri);
-              const newVote = new Vote({votes});
+              console.log('Uploading new vote_uri:', vote.vote_uri);
+              const newVote = new Vote({data: vote});
               newVote.save(voteCb);
             });
           }, sCb);
@@ -39,8 +39,8 @@ module.exports = importData = (cb) => {
           }
         
           const contributions = data.results;
-          async.eachSeries(contributions, (contribution, contributionCb) => {
-            Contribution.find({'contributions.fec_uri': contributions.fec_uri}).lean(true).exec((err, contributions) => {
+          async.eachSeries(data.results, (contribution, contributionCb) => {
+            Contribution.find({'data.fec_uri': contribution.fec_uri}).lean(true).exec((err, contributions) => {
               if (err) {
                 return contributionCb(err);
               }
@@ -49,8 +49,8 @@ module.exports = importData = (cb) => {
                 console.log('Skipping this contribution upload becase we have an existing entry for fec_uri:', contribution.fec_uri);
                 return setImmediate(contributionCb);
               }
-              console.log('Uploading new fec_uri:', contributions.fec_uri);
-              const newContribution = new Contribution({contributions});
+              console.log('Uploading new fec_uri:', contribution.fec_uri);
+              new Contribution({data: contribution});
               newContribution.save(contributionCb);
             })
           }, sCb)
