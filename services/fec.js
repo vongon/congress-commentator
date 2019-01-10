797be810d8f0a4317668a9442a0c04b85ff65174
Matt Oliver <mattoliver.mattoliver@gmail.com>
2019-01-09 23:25:24 -0600

    remove logs

--- services/fec.js
+++ services/fec.js
@@ -1,4 +1,3 @@
-// const request = require('request-promise');
 const request = require('request');
 const config = require('../config');
 
@@ -8,15 +7,6 @@ exports.getPacContributions = getPacContributions = (cb) => {
 		url: `https://api.open.fec.gov/v1/schedules/schedule_a/?api_key=` + config.fec.api_key + `&sort_hide_null=false&sort_nulls_last=true&sort=-contribution_receipt_date&per_page=100&committee_id=` + config.fec.committee_id + `&two_year_transaction_period=2018&is_individual=false`,
 		json: true
 	}
-
-	// request(options)
-	//   .then((response) => {  
-	//     return cb(null, response)
-	//     process.stdout.write('Loading FEC receipts data for ' + config.congressPerson.name + ', ' + config.fec.committee_id);
-	//   })
-	//   .catch((err) => {
-	//   	console.log(err)
-	//   })	
 
 	request(options, (err, response, body) => {
 	  if (err) {
