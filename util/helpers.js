c966238387931f1ffacb2be0d92f2a9e3de881a1
Matt Oliver <mattoliver.mattoliver@gmail.com>
2019-01-26 11:40:29 -0600

    update dictionary :memo:

--- util/helpers.js
+++ util/helpers.js
@@ -67,8 +67,7 @@ const handleDonorName = (str) => {
    ' For Rural Electrification (Acre)': "",
    ' - Usaa Employee Pac': "",
    '/International Union Of Operating Engineers': "",
-   'The National Rural Electric Cooperative Association Action Committee For Rural Electrification (Acre)': "Nat'l. Rural Electric Co-op Assn.",
-   'Farmers Group, Inc., Farmers Insurance Exchange, Fire Insurance Exchange And Truck Insurance Exchange Political Action Committee Aka Farmers Insurance Pac': "Farmers Group, Inc.",
+   'The National Rural Electric Cooperative Association Action Committee For Rural Electrification (Acre)': "Nat'l. Rural Electric Co-op",
    'Farmers Group, Inc., Farmers Insurance Exchange, Fire Insurance Exchange And Truck Insurance Exch...': "Farmers Insurance PAC"
   };
   // remove anything in parentheses
