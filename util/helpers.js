2bde8082a79403c26043fb71f2d456e4f390e21d
Matt Oliver <mattoliver.mattoliver@gmail.com>
2019-02-04 09:51:57 -0600

    update utils :wrench:

--- util/helpers.js
+++ util/helpers.js
@@ -74,7 +74,8 @@ const handleDonorName = (str) => {
    '/International Union Of Operating Engineers': "",
    "The National Rural Electric Cooperative Association Action Committee For Rural Electrifica": "Nat'l. Rural Electric Co-op",
    'Farmers Group, Inc., Farmers Insurance Exchange, Fire Insurance Exchange And Truck Insurance Exch...': "Farmers Insurance PAC",
-   'American Fuels And Petrochemical Manufacturers Association Political Action Committee (Afp': "American Fuels & Petrochemical Manufacturers Assn. PAC",
+   'American Fuels And Petrochemical Manufacturers Association Political Action Committee': "American Fuels & Petrochemical Manufacturers Assn. PAC",
+   '(Afp': "",
    'Texas Association For Home Care & Hospice, Inc. Texas Home Care & Hospice PAC- Federal': "",
    'Political Action Comm': "PAC"
   };
