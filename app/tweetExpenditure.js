d2c7b63954e127bcf878f358636f97f3ed37f4c5
Matt Oliver <mattoliver.mattoliver@gmail.com>
2019-02-19 18:03:58 -0600

    wip :construction:

--- app/tweetExpenditure.js
+++ app/tweetExpenditure.js
@@ -6,8 +6,7 @@ const propublicaService = require('../services/propublica');
 const twitterService = require('../services/twitter');
 const bitlyService = require('../services/bitly')
 
-const Vote = require('../models/vote');
-const PACContribution = require('../models/pacContribution');
+const Vote = require('../models/expenditure');
 
 const okayToTweet = require('../util/helpers').okayToTweet;
 const handleNullValues = require('../util/helpers').handleNullValues;
@@ -15,63 +14,63 @@ const handleDonorName = require('../util/helpers').handleDonorName;
 const toProperCase = require('../util/helpers').toProperCase; 
 
 
-module.exports = tweetContribution = (cb) => {
-    PACContribution.findOne({
+module.exports = tweetExpenditure = (cb) => {
+    Expenditure.findOne({
       tweetedAt: null, 
       'data.committee_id': config.fec.committee_id
-    }).sort({createdAt: 1}).exec((err, contribution) => {
+    }).sort({createdAt: 1}).exec((err, expenditure) => {
       if (err) {
         return cb(err);
       }
       // if there's nothing there
-      if (!contribution) {
-        console.log('No new PAC contribution data available to tweet')
+      if (!expenditure) {
+        console.log('No new campaign expenditure data available to tweet')
         return cb()
       }
-      if (!okayToTweet(contribution)) {
-        console.log(`Skipping tweeting PAC contribution because it didn't pass validation`);
+      if (!okayToTweet(expenditure)) {
+        console.log(`Skipping tweeting campaign expenditure because it didn't pass validation`);
         return cb();
       }       
       // need to handle Null values so string methods don't break
-      handleNullValues(contribution.data)
+      handleNullValues(expenditure.data)
       // get tweet string + shortened URL
-      getPacTweetString(contribution.data, (err, pacMessage) => {
+      getExpenditureTweetString(expenditure.data, (err, expenditureMessage) => {
         if (err) {
           return cb(err);
         }
-        // now we have pacMessage with shortened url and can tweet:
-        twitterService.tweet(pacMessage, (err) => {
+        // now we have expenditureMessage with shortened url and can tweet:
+        twitterService.tweet(expenditureMessage, (err) => {
           if (err) {
-            console.log('PAC tweet err with contribution._id: ', contribution._id)
-            console.log('problem with this pacMessage: ', pacMessage)
+            console.log('Expenditure tweet err with expenditure._id: ', expenditure._id)
+            console.log('problem with this expenditureMessage: ', expenditureMessage)
             return cb(err);
         }
-        console.log('Tweeting PAC data:', pacMessage) 
-        // save any new PAC data contribution entries to the database
+        console.log('Tweeting expenditure data:', expenditureMessage) 
+        // save any new campaign expenditure entries to the database
         
-        contribution.tweetedAt = new Date();
-        contribution.save(cb);
+        expenditure.tweetedAt = new Date();
+        expenditure.save(cb);
 
-        console.log('PAC data successfully tweeted at', contribution.tweetedAt)
+        console.log('PAC data successfully tweeted at', expenditure.tweetedAt)
       });         
     });
   })
 }
 
-getPacTweetString = (contribution, cb) => {
+getExpenditureTweetString = (expenditure, cb) => {
   const name = config.congressPerson.name;
   const party = config.congressPerson.party;
   const jurisdiction = config.congressPerson.jurisdiction;
   const handle = config.congressPerson.handle;
-  const committee = toProperCase(contribution.committee.name)
-  const donor = toProperCase(contribution.donor_committee_name)
-  const abbrevDonor = handleDonorName(donor);
-  const loadDate = moment(contribution.load_date).format('YYYY-MM-DD');
-  const amount = contribution.contribution_receipt_amount.toLocaleString();
-  const donorDescription = toProperCase(contribution.entity_type_desc)
-  const donorState = contribution.contributor_state;
-  const donorCity = toProperCase(contribution.contributor_city)
-  const pdf = contribution.pdf_url;
+  const committee = toProperCase(expenditure.committee.name)
+  const recipient = toProperCase(expenditure.recipient_name)
+  const abbrevRecipient = handleRecipientName(recipient);
+  const loadDate = moment(expenditure.load_date).format('YYYY-MM-DD');
+  const amount = expenditure.expenditure_receipt_amount.toLocaleString();
+  const recipientDescription = toProperCase(expenditure.disbursement_description)
+  const recipientState = expenditure.recipient_state;
+  const recipientCity = toProperCase(expenditure.recipient_city)
+  const pdf = expenditure.pdf_url;
   
   bitlyService.shortenUrl(pdf, (err, shortUrl) => {
     if (err) {
@@ -80,9 +79,9 @@ getPacTweetString = (contribution, cb) => {
     // handle bitly response data:
     var json = JSON.parse(shortUrl);    
     const shortLink = json.data.url;
-    const pacMessage = `On ${loadDate}, "${committee}" reported a $${amount} contribution to ${handle} (${party}-${jurisdiction}) from "${abbrevDonor}", a(n) ${donorDescription} registered in ${donorCity}, ${donorState}.\n\n🔎 : ${shortLink}`;
+    const expenditureMessage = `On ${loadDate}, ${handle} (${party}-${jurisdiction}) "${committee}" reported a $${amount} expenditure to "${abbrevRecipient}", in ${recipientCity}, ${recipientState} for ${recipientDescription}.\n\n🔎 : ${shortLink}`;
 
-    return cb(null, pacMessage);
+    return cb(null, expenditureMessage);
   })
 }
 
