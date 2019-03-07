3e82982eb1ba8e2270e7a92c25beb7a550acba23
Matt Oliver <mattoliver.mattoliver@gmail.com>
2019-03-07 08:14:41 -0600

    update processData

--- app/processData.js
+++ app/processData.js
@@ -120,8 +120,8 @@ const getMemeBottomString = (vote) => {
   }
 
   // super long title
-  if (title.length > 250) {
-    var abbrevTitle = trimString(title, 225)
+  if (title.length > 300) {
+    var abbrevTitle = trimString(title, 250)
     var bottomText = abbrevTitle + '. The bill ' + vote.result + ' on ' + vote.date + '.'; 
     return bottomText
   }
