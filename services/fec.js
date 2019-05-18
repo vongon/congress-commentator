1eb3d10fe21ee60933bd1c5491ac6124cd4a8227
TheeMattOliver <mattoliver.mattoliver@gmail.com>
2019-05-18 10:30:35 -0500

    update fec service to pull 2020 data

--- services/fec.js
+++ services/fec.js
@@ -4,7 +4,7 @@ const config = require('../config');
 exports.getPacContributions = getPacContributions = (cb) => {
 	const options = {
 		method: 'GET',
-		url: `https://api.open.fec.gov/v1/schedules/schedule_a/?api_key=` + config.fec.api_key + `&sort_hide_null=false&sort_nulls_last=true&sort=-contribution_receipt_date&per_page=100&committee_id=` + config.fec.committee_id + `&two_year_transaction_period=2018&is_individual=false`,
+		url: `https://api.open.fec.gov/v1/schedules/schedule_a/?api_key=` + config.fec.api_key + `&sort_hide_null=false&sort_nulls_last=true&sort=-contribution_receipt_date&per_page=100&committee_id=` + config.fec.committee_id + `&two_year_transaction_period=2020&is_individual=false`,
 		json: true
 	}
 
@@ -21,7 +21,7 @@ exports.getPacContributions = getPacContributions = (cb) => {
 exports.getIndivContributions = getIndivContributions = (cb) => {
 	const options = {
 		method: 'GET',
-		url: `https://api.open.fec.gov/v1/schedules/schedule_a/?api_key=` + config.fec.api_key + `&sort_hide_null=false&sort_nulls_last=true&sort=-contribution_receipt_date&per_page=100&committee_id=` + config.fec.committee_id + `&two_year_transaction_period=2018&is_individual=true`,
+		url: `https://api.open.fec.gov/v1/schedules/schedule_a/?api_key=` + config.fec.api_key + `&sort_hide_null=false&sort_nulls_last=true&sort=-contribution_receipt_date&per_page=100&committee_id=` + config.fec.committee_id + `&two_year_transaction_period=2020&is_individual=true`,
 		json: true
 	}
 
@@ -38,7 +38,7 @@ exports.getIndivContributions = getIndivContributions = (cb) => {
 exports.getCampaignExpenditures = getCampaignExpenditures = (cb) => {
 	const options = {
 		method: 'GET',
-		url: `https://api.open.fec.gov/v1/schedules/schedule_b/?api_key=` + config.fec.api_key + `&sort_hide_null=false&sort_nulls_last=true&sort=-disbursement_date&per_page=100&committee_id=` + config.fec.committee_id + `&two_year_transaction_period=2018`,
+		url: `https://api.open.fec.gov/v1/schedules/schedule_b/?api_key=` + config.fec.api_key + `&sort_hide_null=false&sort_nulls_last=true&sort=-disbursement_date&per_page=100&committee_id=` + config.fec.committee_id + `&two_year_transaction_period=2020`,
 		json: true
 	}
 
