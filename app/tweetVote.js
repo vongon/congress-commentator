3ea05f864ef6491bb416cc9e1f3adafccb4d0572
Matt Oliver <mattoliver.mattoliver@gmail.com>
2021-08-22 11:49:09 -0500

    back from the dead

--- app/tweetVote.js
+++ app/tweetVote.js
@@ -48,63 +48,63 @@ module.exports = tweetVote = (cb) => {
           console.log('Vote data successfully tweeted at', vote.tweetedAt)
         });
       });
-      // });
     });
-  };
 
-  // Votes tweet 
-  const getTweetString = (vote) => {
-    // console.log('This vote: ', vote)
-    // Speaker vote has no bill number and vote totals were strings
-    if (vote.question == "Election of the Speaker") {
+
+    // Votes tweet 
+    const getTweetString = (vote) => {
+      // console.log('This vote: ', vote)
+      // Speaker vote has no bill number and vote totals were strings
+      if (vote.question == "Election of the Speaker") {
+        var handle = config.congressPerson.handle
+        var abbreviatedBillQuestion = vote.data.question;
+        var name = config.congressPerson.name;
+        var party = config.congressPerson.party;
+        var jurisdiction = config.congressPerson.jurisdiction;
+        var position = vote.data.position;
+        var pelosiCount = vote.data.total["Pelosi"];
+        var mcCarthyCount = vote.data.total["McCarthy"];
+        var otherCount = 0;
+        Object.keys(vote.data.total).map((item) => {
+          if (!(item == "Pelosi" || item == "McCarthy")) {
+            console.log(item, vote.data.total[item]);
+            otherCount += vote.data.total[item];
+          }
+        })
+        var notCount = vote.data.total["Not Voting"];
+        var presentCount = vote.data.total["Present"];
+        var result = vote.data.result
+        var voteDate = vote.data.date;
+
+        var message = `On "${abbreviatedBillQuestion}", ${name} (${handle} ${party}-${jurisdiction}) voted "${position}". \n\n
+        ${pelosiCount} member(s) voted "Pelosi". ${mcCarthyCount} member(s) voted "McCarthy". ${notCount} not voting, ${presentCount} "Present", ${otherCount} voted for other candidates.\n\n
+        Result: ${result} elected Speaker of the House on ${voteDate}.`;
+
+
+        return message;
+      }
+
       var handle = config.congressPerson.handle
-      var abbreviatedBillQuestion = vote.data.question;
+      var abbreviatedBillQuestion = trimString(vote.data.question, 25)
+      var abbreviatedBillDescription = trimString(vote.data.description, 45)
+      var billNumber = vote.data.bill.number;
       var name = config.congressPerson.name;
       var party = config.congressPerson.party;
       var jurisdiction = config.congressPerson.jurisdiction;
       var position = vote.data.position;
-      var pelosiCount = vote.data.total["Pelosi"];
-      var mcCarthyCount = vote.data.total["McCarthy"];
-      var otherCount = 0;
-      Object.keys(vote.data.total).map((item) => {
-        if (!(item == "Pelosi" || item == "McCarthy")) {
-          console.log(item, vote.data.total[item]);
-          otherCount += vote.data.total[item];
-        }
-      })
-      var notCount = vote.data.total["Not Voting"];
-      var presentCount = vote.data.total["Present"];
+      var yesCount = vote.data.total.yes;
+      var noCount = vote.data.total.no;
+      var notCount = vote.data.total.not_voting;
       var result = vote.data.result
       var voteDate = vote.data.date;
+      // need to remove extension and any quotes to embed img in tweet
+      var imgurUrl = JSON.stringify(vote.imgur.url)
+      imgurUrl = imgurUrl.replace(/\.[^/.]+$/, "")
+      imgurUrl = imgurUrl.replace(/^"/, "");
 
-      var message = `
-On "${abbreviatedBillQuestion}", ${name} (${handle} ${party}-${jurisdiction}) voted "${position}". \n\n
-${pelosiCount} member(s) voted "Pelosi". ${mcCarthyCount} member(s) voted "McCarthy". ${notCount} not voting, ${presentCount} "Present", ${otherCount} voted for other candidates.\n\n
-Result: ${result} elected Speaker of the House on ${voteDate}.`;
+      var message = `"${abbreviatedBillQuestion}" on ${billNumber}, ${name} (${handle} ${party}-${jurisdiction}) voted "${position}".\n\nShort Description: '${abbreviatedBillDescription}'.\n\n${yesCount} member(s) voted "Yes". ${noCount} voted "No". ${notCount} not voting. Result: ${result} ${voteDate}.\n${imgurUrl}`;
 
       return message;
     }
-
-    var handle = config.congressPerson.handle
-    var abbreviatedBillQuestion = trimString(vote.data.question, 25)
-    var abbreviatedBillDescription = trimString(vote.data.description, 45)
-    var billNumber = vote.data.bill.number;
-    var name = config.congressPerson.name;
-    var party = config.congressPerson.party;
-    var jurisdiction = config.congressPerson.jurisdiction;
-    var position = vote.data.position;
-    var yesCount = vote.data.total.yes;
-    var noCount = vote.data.total.no;
-    var notCount = vote.data.total.not_voting;
-    var result = vote.data.result
-    var voteDate = vote.data.date;
-    // need to remove extension and any quotes to embed img in tweet
-    var imgurUrl = JSON.stringify(vote.imgur.url)
-    imgurUrl = imgurUrl.replace(/\.[^/.]+$/, "")
-    imgurUrl = imgurUrl.replace(/^"/, "");
-
-    var message = `"${abbreviatedBillQuestion}" on ${billNumber}, ${name} (${handle} ${party}-${jurisdiction}) voted "${position}".\n\nShort Description: '${abbreviatedBillDescription}'.\n\n${yesCount} member(s) voted "Yes". ${noCount} voted "No". ${notCount} not voting. Result: ${result} ${voteDate}.\n${imgurUrl}`;
-
-    return message;
-  }
-
+  })
+}
